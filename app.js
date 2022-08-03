c2b51813f7a6a040cfe164ac580f657973b9a332
Elliot <elliotcs30@gmail.com>
2022-08-03 20:43:55 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
+// 載入 express-handlebars 並命名為 exphbs
+const exphbs = require('express-handlebars')
 const app = express()
 
 const mongoose = require('mongoose') // 載入 mongoose
@@ -16,9 +18,15 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+// 建立名為 handlebars 的樣版引擎，並傳入 exphbs 與相關參數
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
+// 透過 app.set 告訴 express 說要設定 view engine 是 handlebars
+// 啟用樣版引擎 handlebars
+app.set('view engine', 'handlebars')
+
 // 設定首頁路由
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index')
 })
 
 // 設定 port 3000
