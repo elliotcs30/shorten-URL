de3748715b6bb3d868d9d1ba0843133bbf7a268f
Elliot <elliotcs30@gmail.com>
2022-08-03 22:56:14 +0800

    feat: modify index.handlebars

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require('express')
 // 載入 express-handlebars 並命名為 exphbs
 const exphbs = require('express-handlebars')
+// 載入 shorten URL model
+const ShortURL = require('./models/shortURL')
 const app = express()
 
 const mongoose = require('mongoose') // 載入 mongoose
@@ -24,9 +26,17 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
 // 啟用樣版引擎 handlebars
 app.set('view engine', 'handlebars')
 
-// 設定首頁路由
+
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+// 設定首頁路由
+app.get('/', (req, res) => {
+  ShortURL.find() // 取出 Shorten URL model 裡的所有資料
+  .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+  .then(shortURLs => res.render('index', { shortURLs })) // 將資料傳給 index 樣板
+  .catch(error => console.log(error)) // 錯誤處理
 })
 
 // 設定 port 3000
